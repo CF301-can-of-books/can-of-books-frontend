5c67b2579e61ef1a58332eecbf9a0cd8c687c4ad
stefrie <sriehle@gmail.com>
2021-08-31 14:29:31 -0700

    render BestBooks, map through books

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
 
 const server = process.env.REACT_APP_BASE_URL;
 
@@ -17,11 +18,12 @@ class BestBooks extends React.Component {
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
   async componentDidMount() {
 	const response = await axios.get(`${server}/books?email=bill@microsoft.com`);
-	alert(JSON.stringify(response.data));
+	const newBook = response.data;
+	const books = [...this.state.books, newBook];
+	this.setState({ books }); 
   }
 
   render() {
-
     /* TODO: render user's books in a Carousel */
 
     return (
@@ -29,7 +31,23 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+			<Carousel key={0}>
+				{this.state.books.map(book => {
+					return (
+					<Carousel.Item key={book._id}>
+					{/* <img
+						className="d-block w-100"
+						src="holder.js/800x400?text=First slide&bg=373940"
+						alt="First slide"
+					/> */}
+					<Carousel.Caption>
+						<h3>{book.title}</h3>
+						<p>{book.description}</p>
+					</Carousel.Caption>
+				</Carousel.Item>)
+				}) 
+				};
+			</Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
