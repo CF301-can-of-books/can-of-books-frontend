f9f10ffd3b8868fc98ef537468dedd639458c4e1
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-09-01 00:40:22 -0700

    adds last tasks of lab11 card3

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './Header.css';
+import LogoutButton from './LogoutButton';
 
 class Header extends React.Component {
   render() {
@@ -10,8 +11,15 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
 		<NavItem><Link to="/add" className="nav-link">Add a Book</Link></NavItem>
-		<NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>
-		<NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+    <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+    {this.props.user ? 
+    <>
+		<NavItem><Link to="/profile" id="profileNav" className="nav-link">Profile</Link></NavItem>
+    <LogoutButton />
+    </>
+    :
+    <></>
+  }
         {/* TODO: if the user is logged in, render a navigation link to profile page */}
         {/* TODO: if the user is logged in, render the `LogoutButton` */}
       </Navbar>
--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import './login.css';
+import './Login.css';
+import LoginButton from './LoginButton';
+import LoginForm from './LoginForm';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null,
+    }
+  }
+
+
+  handleButtonSubmit = () => {
+    let userObj = {
+      email: this.state.email,
+      password: this.state.password,
+    }
+    this.props.onLogin(userObj);
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    })
+  }
+
+  handlePassChange = (event) => {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+ 
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem', marginTop: '50px'}}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
           <Card.Text>
             Click Below to Log In
           </Card.Text>
+          <LoginForm onEmailChange={this.handleEmailChange} onPassChange={this.handlePassChange}/>
+          <LoginButton onLogin={this.handleButtonSubmit}/>
           {/* TODO: add a `LoginButton` component here that will log the user in */}
         </Card.Body>
       </Card>
